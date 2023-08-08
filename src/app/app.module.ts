f81830b6b6b9c0917ce4ecae47edb18d174d83ca
Eduardo P <edtvports@gmail.com>
2023-08-08 18:37:48 -0300

    Corrected bugs, removed useless component and started home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
-import { CookieService } from "ngx-cookie-service";
-import { HomeComponent } from './pages/home/home.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { LoginSignupComponent } from './pages/login-signup/login-signup.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CookieService} from "ngx-cookie-service";
+import {HomeComponent} from './pages/home/home.component';
+import {ProductsComponent} from './pages/products/products.component';
+import {CartComponent} from './pages/cart/cart.component';
+import {ProfileComponent} from './pages/profile/profile.component';
+import {LoginSignupComponent} from './pages/login-signup/login-signup.component';
+import {LoginComponent} from './components/login/login.component';
 import {FormsModule} from "@angular/forms";
-import { SignupComponent } from './components/signup/signup.component';
-import { ProductCategoryComponent } from './components/product-category/product-category.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {ProductCategoryComponent} from './components/product-category/product-category.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {NgOptimizedImage} from "@angular/common";
-import { BackBarComponent } from './components/back-bar/back-bar.component';
 
 @NgModule({
   declarations: [
@@ -29,8 +28,7 @@ import { BackBarComponent } from './components/back-bar/back-bar.component';
     LoginComponent,
     SignupComponent,
     ProductCategoryComponent,
-    NavbarComponent,
-    BackBarComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -42,4 +40,5 @@ import { BackBarComponent } from './components/back-bar/back-bar.component';
   providers: [CookieService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
