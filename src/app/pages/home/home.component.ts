a13100f30557b3bb6b4d0356e6c64629a55a3c90
Eduardo P <edtvports@gmail.com>
2023-08-10 18:40:39 -0300

    Minor tweaks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   public carrouselIndex = 1
 
   public readonly SELECTED_CARROUSEL: string = "w-6 bg-dark"
-  public readonly UNSELECTED_CARROUSEL: string = "w-3 bg-light"
+  public readonly UNSELECTED_CARROUSEL: string = "w-3 bg-light hover:bg-lightHover"
 
   private readonly CARROUSEL_SECONDS = 8000
 
