a3ca511356e2435b778d94d69652080abb263de3
Eduardo P <edtvports@gmail.com>
2023-08-20 10:03:52 -0300

    Removed unused variable

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -8,8 +8,6 @@ import Category from "../../types/category";
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  private readonly invalidChars = ['-', '+', 'e', 'E', '.'];
-
   public categoryFilters: string[] = []
 
   public inactiveCategories: string[] = []
