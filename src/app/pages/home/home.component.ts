48526f22e97614cc8109350d2be39193d10d9ffe
Eduardo P <edtvports@gmail.com>
2023-08-21 09:55:22 -0300

    Toast warning about the back-end cold start

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import Product from "../../types/product";
 import {ContentService} from "../../services/content.service";
+import {animate, style, transition, trigger} from "@angular/animations";
+import {ToastComponent} from "../../modals/toast/toast.component";
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,8 @@ import {ContentService} from "../../services/content.service";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('toast') toast?: ToastComponent
+
   public carrouselIndex = 1
 
   public readonly SELECTED_CARROUSEL: string = "w-6 bg-dark"
@@ -22,6 +26,7 @@ export class HomeComponent implements OnInit {
   }
 
   async ngOnInit() {
+    this.showWarning()
     this.startCarrouselRotation()
     this.productExcerpt = await this.contentService.getAllProducts(this.AMOUNT_OF_PRODUCTS)
   }
@@ -32,5 +37,10 @@ export class HomeComponent implements OnInit {
       if (this.carrouselIndex + 1 == 4) this.carrouselIndex = 1
       else this.carrouselIndex++
     }
+  }
+
+  private async showWarning() {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    this.toast?.showMessage("O Back-end pode levar cerca de 3 minutos para iniciar")
   }
 }
